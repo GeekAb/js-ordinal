ed8b7768822aae67817bac1cb4cba1ef39dd5461
Abhishek <nit.abhi85@gmail.com>
2018-09-19 00:32:22 +0530

    Adding functions to generate ordinal and retrun it with number

---
+++ ordinal.js
@@ -0,0 +1,29 @@
+"use strict";
+
+//Array to hold on to ordinals
+const ordinalsArray = ['th', 'st', 'nd', 'rd'];
+
+const ordinal = {
+
+    ordinalSuffix (num) {
+        //Get reminder of number by hundred so that we can counter number between 11-19
+        let offset = num % 100;
+      
+        // Calculate position of ordinal to be used. Logic : Array index is calculated based on defined values.
+        let ordinalPos = ordinalsArray[ ( offset-20 )%10 ] || ordinalsArray[ offset ] || ordinalsArray[0];
+    
+        //Return suffix
+        return ordinalPos;
+    },
+
+    toOrdinal (num) {
+
+        //If number is zero no need to spend time on calculation
+        if(num === 0)
+            return num.toString();
+        
+        return num.toString() + this.ordinalSuffix(num);
+    }
+}
+
+module.exports = ordinal;
