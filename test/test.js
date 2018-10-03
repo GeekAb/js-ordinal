ed8ffb039846b091eae89305b73864cb678739c9
Abhishek <nit.abhi85@gmail.com>
2018-10-03 16:41:21 +0530

    Adding test cases for valid and invalid cases

---
+++ test/test.js
@@ -0,0 +1,47 @@
+const expect = require('chai').expect;
+const o = require('../index.js');
+
+describe('ordinalOf_1()', function () {
+  it('should return number with its ordinal', function () {
+    const x = 1;
+    const xOrdinal = '1st';
+    var y = o.toOrdinal(x);
+    expect(y).to.be.equal(xOrdinal);
+  });
+});
+
+describe('ordinalOf_a()', function () {
+    it('should return error', function () {
+      const x = 'a';
+      const xOrdinal = 'a is not a valid number';
+      var y = o.toOrdinal(x);
+      expect(y).to.be.equal(xOrdinal);
+    });
+});
+
+describe('ordinalOf_11()', function () {
+    it('should return number with its ordinal', function () {
+      const x = 11;
+      const xOrdinal = '11th';
+      var y = o.toOrdinal(x);
+      expect(y).to.be.equal(xOrdinal);
+    });
+});
+
+describe('ordinalOf_13()', function () {
+    it('should return number with its ordinal', function () {
+      const x = 13;
+      const xOrdinal = '13th';
+      var y = o.toOrdinal(x);
+      expect(y).to.be.equal(xOrdinal);
+    });
+});
+
+describe('ordinalOf_21()', function () {
+    it('should return number with its ordinal', function () {
+      const x = 21;
+      const xOrdinal = '21st';
+      var y = o.toOrdinal(x);
+      expect(y).to.be.equal(xOrdinal);
+    });
+});
